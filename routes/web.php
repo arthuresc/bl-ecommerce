fed22458f2bf74bb99fb083b3c8f86cf55b1d0fa
Arthur Escalera <arthurescalera@gmail.com>
2021-04-12 22:32:54 -0300

    Rotas alteradas

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,20 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::resource('/product', ProductsController::class);
+Route::get('/trash/product', [ProductsController::class, 'trash'])->name('product.trash');
+Route::patch('/product/restore/{id}', [ProductsController::class, 'restore'])->name('product.restore');
+
+Route::resource('/tag', TagsController::class);
+Route::get('/trash/tag', [TagsController::class, 'trash'])->name('tag.trash');
+Route::patch('/tag/restore/{id}', [TagsController::class, 'restore'])->name('tag.restore');
+
+Route::resource('/tagGroup', TagsGroupsController::class);
+Route::get('/trash/tagGroup', [TagsGroupsController::class, 'trash'])->name('tagGroup.trash');
+Route::patch('/tagGroup/restore/{id}', [TagsGroupsController::class, 'restore'])->name('tagGroup.restore');
+
+Route::resource('/category', CategoriesController::class);
+Route::get('/trash/category', [CategoriesController::class, 'trash'])->name('category.trash');
+Route::patch('/category/restore/{id}', [CategoriesController::class, 'restore'])->name('category.restore');
+
 require __DIR__.'/auth.php';
